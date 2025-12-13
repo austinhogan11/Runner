007bb652ba00d15274526d28387b33318406471b
Austin Hogan <austinhogandev@gmail.com>
2025-12-13 14:59:50 -0500

    set up Azure ACR

--- infra/azure/dev/main.tf
+++ infra/azure/dev/main.tf
@@ -6,3 +6,16 @@ resource "azurerm_resource_group" "runner" {
     env = var.environment
   })
 }
+
+resource "azurerm_container_registry" "runner" {
+  name                = "runneracr${replace(var.environment, "-", "")}"
+  resource_group_name = azurerm_resource_group.runner.name
+  location            = azurerm_resource_group.runner.location
+
+  sku           = "Basic"
+  admin_enabled = false
+
+  tags = merge(var.tags, {
+    env = var.environment
+  })
+}
--- infra/azure/dev/outputs.tf
+++ infra/azure/dev/outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = azurerm_resource_group.runner.location
 }
+
+output "acr_login_server" {
+  value = azurerm_container_registry.runner.login_server
+}
