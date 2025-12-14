2f6ff8b891f9d294e57573d6bbc2a75355180f5a
Austin Hogan <austinhogandev@gmail.com>
2025-12-13 22:54:20 -0500

    Build azure infra

--- infra/azure/dev/main.tf
+++ infra/azure/dev/main.tf
@@ -1,79 +1,74 @@
-# resource "azurerm_resource_group" "runner" {
-#   name     = var.resource_group_name
-#   location = var.location
+terraform {
+  required_version = ">= 1.6.0"
 
-#   tags = merge(var.tags, {
-#     env = var.environment
-#   })
-# }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
 
-# resource "azurerm_container_registry" "runner" {
-#   name                = "runneracr${replace(var.environment, "-", "")}"
-#   resource_group_name = azurerm_resource_group.runner.name
-#   location            = azurerm_resource_group.runner.location
+provider "azurerm" {
+  features {}
+}
 
-#   sku           = "Basic"
-#   admin_enabled = false
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
 
-#   tags = merge(var.tags, {
-#     env = var.environment
-#   })
-# }
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "runner-${var.environment}-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = var.tags
+}
 
-# resource "azurerm_log_analytics_workspace" "runner" {
-#   name                = "runner-law-${var.environment}"
-#   location            = azurerm_resource_group.runner.location
-#   resource_group_name = azurerm_resource_group.runner.name
-#   sku                 = "PerGB2018"
-#   retention_in_days   = 30
+resource "azurerm_container_registry" "acr" {
+  name                = "runneracr${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+  tags                = var.tags
+}
 
-#   tags = merge(var.tags, { env = var.environment })
-# }
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "runner-aks-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "runner-aks-${var.environment}"
 
-# resource "azurerm_virtual_network" "runner" {
-#   name                = "runner-vnet-${var.environment}"
-#   location            = azurerm_resource_group.runner.location
-#   resource_group_name = azurerm_resource_group.runner.name
-#   address_space       = ["10.10.0.0/16"]
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+    vm_size    = var.aks_node_vm_size
+  }
 
-#   tags = merge(var.tags, { env = var.environment })
-# }
+  identity {
+    type = "SystemAssigned"
+  }
 
-# resource "azurerm_subnet" "aks" {
-#   name                 = "aks-subnet-${var.environment}"
-#   resource_group_name  = azurerm_resource_group.runner.name
-#   virtual_network_name = azurerm_virtual_network.runner.name
-#   address_prefixes     = ["10.10.1.0/24"]
-# }
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  }
 
-# resource "azurerm_kubernetes_cluster" "runner" {
-#   name                = "runner-aks-${var.environment}"
-#   location            = azurerm_resource_group.runner.location
-#   resource_group_name = azurerm_resource_group.runner.name
-#   dns_prefix          = "runner-aks-${var.environment}"
+  tags = var.tags
+}
 
-#   kubernetes_version  = null # let Azure choose default stable
+# Allow AKS kubelet to pull from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
 
-#   default_node_pool {
-#     name           = "system"
-#     node_count     = 1
-#     vm_size        = var.aks_node_vm_size
-#     vnet_subnet_id = azurerm_subnet.aks.id
-#   }
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-
-#   oms_agent {
-#     log_analytics_workspace_id = azurerm_log_analytics_workspace.runner.id
-#   }
-
-#   tags = merge(var.tags, { env = var.environment })
-# }
-
-# resource "azurerm_role_assignment" "aks_acr_pull" {
-#   scope                = azurerm_container_registry.runner.id
-#   role_definition_name = "AcrPull"
-#   principal_id         = azurerm_kubernetes_cluster.runner.kubelet_identity[0].object_id
-# }
+  # kubelet identity object id:
+  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
--- infra/azure/dev/outputs.tf
+++ infra/azure/dev/outputs.tf
@@ -1,15 +1,15 @@
-# output "resource_group_name" {
-#   value = azurerm_resource_group.runner.name
-# }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
 
-# output "resource_group_location" {
-#   value = azurerm_resource_group.runner.location
-# }
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
 
-# output "acr_login_server" {
-#   value = azurerm_container_registry.runner.login_server
-# }
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
 
-# output "aks_name" {
-#   value = azurerm_kubernetes_cluster.runner.name
-# }
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
