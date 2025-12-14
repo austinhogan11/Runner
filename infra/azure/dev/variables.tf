9b8a547398fc08b5955f95e27c4e770d3c9f9d18
Austin Hogan <austinhogandev@gmail.com>
2025-12-14 14:19:58 -0500

    Add postgres DB

--- infra/azure/dev/variables.tf
+++ infra/azure/dev/variables.tf
@@ -41,3 +41,34 @@ variable "acr_sku" {
   description = "Azure Container Registry SKU"
   default     = "Basic"
 }
+
+# Postgres (Flexible Server) settings
+variable "postgres_admin_user" {
+  type        = string
+  description = "Postgres admin username"
+  default     = "runneradmin"
+}
+
+variable "postgres_sku_name" {
+  type        = string
+  description = "Flexible Server SKU (e.g., B_Standard_B1ms, GP_Standard_D2s_v3)"
+  default     = "B_Standard_B1ms"
+}
+
+variable "postgres_version" {
+  type        = string
+  description = "Postgres major version"
+  default     = "16"
+}
+
+variable "postgres_db_name" {
+  type        = string
+  description = "Application database name"
+  default     = "runner"
+}
+
+variable "postgres_storage_mb" {
+  type        = number
+  description = "Flexible Server storage in MB"
+  default     = 32768
+}
